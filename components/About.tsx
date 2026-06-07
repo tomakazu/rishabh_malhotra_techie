34623880990a08098668e7323867261d5673deb8
tomakazu <matcortnermat@gmail.com>
2026-06-07 09:42:24 +0530

    Created home page for my portfolio

---
+++ components/About.tsx
@@ -0,0 +1,24 @@
+export default function About() {
+  return (
+    <section className="py-32 px-6">
+      <div className="max-w-4xl mx-auto">
+        <h2 className="text-4xl font-bold mb-8">
+          About Me
+        </h2>
+
+        <p className="text-lg">
+          I am a Software Engineer with 4+ years of
+          experience building robust, scaleable applications
+          using Ruby on Rails. I have experience in mainly working
+          in startups. For more than 3.5 plus years, I have worked in
+          healthtech startups. Currently, working in fintech startup
+          called OneNdf.
+        </p>
+        <p className="text-lg">
+          My interests extend beyond software development into
+          algorithms, system design, linguistics, sports and language learning. 
+        </p>
+      </div>
+    </section>
+  );
+}
